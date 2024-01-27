b26bc349959e2e8c5407dbdb30c3f7804850063d
iStefanAdv <73856861+iStefanAdv@users.noreply.github.com>
2024-01-27 15:48:22 +0200

    Add files via upload

---
+++ RoyalApi.php
@@ -0,0 +1,120 @@
+<?php
+
+/////////////////////////////////////////////
+//===========================================
+//         Api Shield RoyaleHosting        //
+//===========================================
+// Version: 1.0
+// Created by iSt-Panel (Petcu Mihai)
+// Email: developer@ist-panel.ro
+//===========================================
+// (c) iSt-Panel
+//===========================================
+
+
+class RoyalApi {
+
+    var $key = '';
+    var $timeout = 30;
+    var $apiUrl = 'https://shield.royalehosting.net/api';
+    var $token = '';
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    /**************************************************
+     *                  Debug Cods
+     **************************************************/
+    public function ist_debug(string $text, bool $stop = false)
+    {
+        ?>
+            <pre>
+                <code>
+                    <?php print_r($text) ?>
+                </code>
+            </pre>
+        <?php
+
+        if ($stop) {
+            die;
+        }
+    }
+
+    /*************************************************
+    */
+
+    public function jsonEncode(array $data)
+    {
+        return json_encode($data);
+    }
+
+    public function jsonDecode(string $data)
+    {
+        return json_decode($data);
+    }
+
+    private function callShield(string $version, string $route, bool $post = false)
+    {
+
+        $url = $this->apiUrl . "/". $version. "/". $route;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        if ($post) {
+            $postFields = [];
+            $postFields['key'] = $this->key;
+
+            $post = $this->jsonEncode($postFields);
+
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+        } else {
+            $header = [
+                'token:' . $this->token
+            ];
+
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+        }
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        $response = $this->jsonDecode($data);
+
+        return $response;
+    }
+
+    public function loginShield(string $version)
+    {
+        $data = $this->callShield($version, 'auth/api', true);
+        $this->token = $data->data->token;
+        return $this;
+    }
+
+    public function listAttack(string $version)
+    {
+        $response = $this->callShield($version, 'attacks?chunk=0');
+        return $response->data->attacks;
+    }
+
+    public function getAttack(string $version, int $attackId)
+    {
+        $response = $this->callShield($version, "attacks/". $attackId);
+        return $response;
+    }
+
+    public function listIps(string $version)
+    {
+        $response = $this->callShield($version, "ips");
+        return $response;
+    }
+}
